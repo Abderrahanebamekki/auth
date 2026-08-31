b8a3ea6af505f82e000a6998b7cf9b47a4b155d4
abdoubmk49 <bamekki.abderrahmane49@gmail.com>
2026-08-31 19:59:39 +0100

    Stage 3: profile route token verification

--- handler/protected.go
+++ handler/protected.go
@@ -2,7 +2,9 @@ package handler
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -17,7 +19,26 @@ func Protected(w http.ResponseWriter, r *http.Request) {
 		})
 		return
 	}
+	token := strings.TrimPrefix(authHeader, "Bearer ")
 
+	req, _ := http.NewRequest("GET", os.Getenv("SUPABASE_URL")+"/auth/v1/user", nil)
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("apikey", os.Getenv("SUPABASE_KEY"))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": "Invalid or expired token",
+		})
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, _ := io.ReadAll(resp.Body)
 	w.WriteHeader(http.StatusOK)
+	w.Write(body)
 
 }
